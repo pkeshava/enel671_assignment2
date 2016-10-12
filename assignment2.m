0baa24102af412d04233c46af203e8dd487544ac
Pouyan <pouyank13@me.com>
2016-10-12 13:40:03 -0600

    add submatrices of R for differenet filter orders

--- assignment2.m
+++ assignment2.m
@@ -1,4 +1,8 @@
 %% ENEL 671 Assingment 2
 %%
 r = [2 1 0.75 0.5 0.25]; 
-R = toeplitz(r);
+R_fifth = toeplitz(r);
+r_second_order = R(1:2,1:2);
+r_third_order = R(1:3,1:3);
+r_fourth_order = R(1:4,1:4);
+
