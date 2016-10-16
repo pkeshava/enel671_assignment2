42924030ad5b5fdff8441fd19a69873e6381b79f
Pouyan <pouyank13@me.com>
2016-10-15 19:48:22 -0600

    Commented codeand finished report. Added mcode package to add matlab code to document

--- assignment2.m
+++ assignment2.m
@@ -1,8 +1,8 @@
-%% ENEL 671 Assingment 2
+%% ENEL 671 Assignment 2
 % Pouyan Keshavarzian
 % ENEL 671
 %%
-% Calculate eigenvalues for each set of 
+% Define autocorrelation matrix
 R = [2 1 0.75 0.5 0.25]; 
 R = toeplitz(R);
 for M=2:5  
@@ -18,16 +18,16 @@ UpBoundStepSize=2./TraceR
 %% Part 2 
 % Calculate weights for each filter order
 
-
+% Autocovarriance matrix p 
 p = [0.5 0.25 0.125 0.0625 0.03125]';
 sigma_squared = 1.0;
-
+% Calculate tap-input vectors for corresponding filter coefficeints
 w2 = inv(R(1:2,1:2))*p(1:2);     
 w3 = inv(R(1:3,1:3))*p(1:3);
 w4 = inv(R(1:4,1:4))*p(1:4);
 w5 = inv(R(1:5,1:5))*p(1:5);
 
-
+% Calculate MMSE for corresponding filter coefficeints
 MMSE2 = sigma_squared - w2'*p(1:2);
 MMSE3 = sigma_squared - w3'*p(1:3);
 MMSE4 = sigma_squared - w4'*p(1:4);
